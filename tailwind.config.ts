a58e29cceb70e5842047db3be74aa728918c7b2c
Bonaventura Galang <77723914+bonaventuragal@users.noreply.github.com>
2024-03-14 10:32:00 +0700

    add custom colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "ash-white": "#DFD9C3",
+        "ash-green": "#668F6F",
+      },
     },
   },
   plugins: [],
